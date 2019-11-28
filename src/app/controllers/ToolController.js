5761c732bab0d32c151be6a82210625a48e4871a
Leonardo Barbosa <leonardo.pb@icloud.com>
2019-11-28 13:30:33 -0300

    Ajuste no controller

--- src/app/controllers/ToolController.js
+++ src/app/controllers/ToolController.js
@@ -44,42 +44,6 @@ module.exports = {
     return res.json({ title, link, description, tags });
   },
 
-  async update(req, res) {
-    const schema = yup.object().shape({
-      title: yup.string().notRequired(),
-      link: yup.string().notRequired(),
-      description: yup
-        .string()
-        .min(10)
-        .notRequired(),
-      tags: yup.array().notRequired(),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: 'Validation Fails' });
-    }
-
-    const { id } = req.params;
-
-    const ToolExists = await Tool.findById(id);
-
-    if (!ToolExists) {
-      return res.status(401).json({ error: 'Tool do not exists' });
-    }
-
-    const { title, link, description, tags } = req.body;
-
-    const titleInUse = await Tool.findOne({ title });
-
-    if (titleInUse) {
-      return res.status(401).json({ error: 'Title in use' });
-    }
-
-    await ToolExists.update({ title, link, description, tags });
-
-    return res.json({ id, title, link, description, tags });
-  },
-
   async delete(req, res) {
     const { id } = req.params;
 
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,5 @@ const ToolController = require('./app/controllers/ToolController');
 routes.get('/tools', ToolController.index);
 routes.post('/tools', ToolController.store);
 routes.delete('/tools/:id', ToolController.delete);
-routes.put('/tools/:id', ToolController.update);
 
 module.exports = routes;
